e5bbbaa92df78c9183c938999d7b366b20dd59f1
Muntasir00 <muntasir690000@gmail.com>
2022-07-03 02:35:21 +0600

    completed

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template 
 from bs4 import BeautifulSoup 
 
-import requests 
+import requests
+app = Flask(__name__)
+@app.route('/',methods=["GET","POST"]) 
+def index():
+    url="https://www.businesstoday.in/technology/news"
+    req = requests.get(url)
 
-url="https://www.businesstoday.in/technology/news"
-req = requests.get(url)
+    soup = BeautifulSoup(req.content,'html.parser')
 
-soup = BeautifulSoup(req.content,'html.parser')
+    outerdata=soup.find_all("div",class_="widget-listing",limit=6)
+    finalnews=""
 
-outerdata=soup.find_all("div",class_="widget-listing",limit=6)
-finalnews=""
-
-for data in outerdata:
-    news = data.div.div.a["title"]
-    finalnews += "\u2022 " + news +"\n"
-print(finalnews)
+    for data in outerdata:
+        news = data.div.div.a["title"]
+        finalnews += "\u2022 " + news +"\n"
+    return render_template("index.html", News=finalnews)
